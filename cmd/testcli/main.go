ba9ceb1d0b5dfaf1b1738c995bdff5b6568eff3e
Ben Schwartz <scben@umich.edu>
2025-10-10 21:22:48 -0400

    third commit

--- cmd/testcli/main.go
+++ cmd/testcli/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "testcli",
+	Use: "testcli",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 0 {
 			// print all the args
@@ -36,8 +36,35 @@ var versionCmd = &cobra.Command{
 	},
 }
 
+var completionCmd = &cobra.Command{
+	Use:       "completion [bash|zsh]",
+	ValidArgs: []string{"bash", "zsh"},
+	Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Run: func(cmd *cobra.Command, args []string) {
+		shell := args[0]
+		switch shell {
+		case "bash":
+			err := rootCmd.GenBashCompletion(os.Stdout)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error generating bash completion: %v\n", err)
+				os.Exit(1)
+			}
+		case "zsh":
+			err := rootCmd.GenZshCompletion(os.Stdout)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error generating zsh completion: %v\n", err)
+				os.Exit(1)
+			}
+		default:
+			fmt.Fprintf(os.Stderr, "Unsupported shell: %s. Use 'bash' or 'zsh'\n", shell)
+			os.Exit(1)
+		}
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(completionCmd)
 }
 
 func main() {
